773cb02ad57ed24106d2633cc96d40ffa8a436f8
brunobat <brunobat@gmail.com>
2022-10-06 19:20:50 +0100

    workflows documentation about configurations Jaeger extension plus documentation skeleton based on extension from the main quarkus repository project structure

---
+++ quarkus-opentelemetry-exporter-jaeger/deployment/src/main/java/io/quarkiverse/opentelemetry/exporter/jaeger/deployment/JaegerExporterProcessor.java
@@ -0,0 +1,40 @@
+package io.quarkiverse.opentelemetry.exporter.jaeger.deployment;
+
+import java.util.function.BooleanSupplier;
+
+import io.quarkiverse.opentelemetry.exporter.jaeger.runtime.JaegerExporterConfig;
+import io.quarkiverse.opentelemetry.exporter.jaeger.runtime.JaegerExporterProvider;
+import io.quarkiverse.opentelemetry.exporter.jaeger.runtime.JaegerRecorder;
+import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.BuildSteps;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.LaunchModeBuildItem;
+
+@BuildSteps(onlyIf = JaegerExporterProcessor.JaegerExporterEnabled.class)
+public class JaegerExporterProcessor {
+
+    static class JaegerExporterEnabled implements BooleanSupplier {
+        JaegerExporterConfig.JaegerExporterBuildConfig jaegerExporterConfig;
+
+        public boolean getAsBoolean() {
+            return jaegerExporterConfig.enabled;
+        }
+    }
+
+    @BuildStep
+    AdditionalBeanBuildItem createBatchSpanProcessor() {
+        return AdditionalBeanBuildItem.builder()
+                .addBeanClass(JaegerExporterProvider.class)
+                .setUnremovable().build();
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.RUNTIME_INIT)
+    void installBatchSpanProcessorForJaeger(JaegerRecorder recorder,
+            LaunchModeBuildItem launchModeBuildItem,
+            JaegerExporterConfig.JaegerExporterRuntimeConfig runtimeConfig) {
+        recorder.installBatchSpanProcessorForJaeger(runtimeConfig, launchModeBuildItem.getLaunchMode());
+    }
+}
