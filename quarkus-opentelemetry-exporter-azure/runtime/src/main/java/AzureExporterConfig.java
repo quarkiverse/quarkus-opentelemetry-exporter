9372687b4eb5a36d81bb898a4e50ae6d9fe610fa
Jean Bisutti <jean.bisutti@gmail.com>
2024-02-20 10:43:27 +0100

    Add Azure configuration

---
+++ quarkus-opentelemetry-exporter-azure/runtime/src/main/java/AzureExporterConfig.java
@@ -0,0 +1,25 @@
+import java.util.Optional;
+
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+import io.quarkus.runtime.annotations.ConvertWith;
+import io.quarkus.runtime.configuration.TrimmedStringConverter;
+
+public class AzureExporterConfig {
+    @ConfigRoot(name = "applicationinsights", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
+    public static class AzureExporterBuildConfig {
+
+    }
+
+    @ConfigRoot(name = "applicationinsights.connection.string", phase = ConfigPhase.RUN_TIME)
+    public static class AzureExporterRuntimeConfig {
+
+        /**
+         * The Azure connection string
+         */
+        @ConvertWith(TrimmedStringConverter.class)
+        //applicationinsights.connection.string
+        public Optional<String> string;
+
+    }
+}
