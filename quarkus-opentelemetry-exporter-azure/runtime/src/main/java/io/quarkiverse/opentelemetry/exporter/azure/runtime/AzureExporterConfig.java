c260cf522e45e617a8e969617ae89014adb7e8f4
brunobat <brunobat@gmail.com>
2024-02-20 10:30:30 +0000

    config

--- quarkus-opentelemetry-exporter-azure/runtime/src/main/java/io/quarkiverse/opentelemetry/exporter/azure/runtime/AzureExporterConfig.java
+++ quarkus-opentelemetry-exporter-azure/runtime/src/main/java/io/quarkiverse/opentelemetry/exporter/azure/runtime/AzureExporterConfig.java
@@ -2,24 +2,25 @@ package io.quarkiverse.opentelemetry.exporter.azure.runtime;
 
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
-import io.quarkus.runtime.annotations.ConvertWith;
 import io.quarkus.runtime.configuration.TrimmedStringConverter;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithConverter;
+import io.smallrye.config.WithName;
 
 public class AzureExporterConfig {
-    @ConfigRoot(name = "applicationinsights", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
     public static class AzureExporterBuildConfig {
 
     }
 
-    @ConfigRoot(name = "applicationinsights.connection.string", phase = ConfigPhase.RUN_TIME)
+    @ConfigMapping(prefix = "quarkus.otel.azure")
+    @ConfigRoot(phase = ConfigPhase.RUN_TIME)
     public static class AzureExporterRuntimeConfig {
 
         /**
-         * The Azure connection string
+         * The Azure connection string.
          */
-        @ConvertWith(TrimmedStringConverter.class)
-        //applicationinsights.connection.string
+        @WithConverter(TrimmedStringConverter.class)
+        @WithName("applicationinsights.connection.string")
         public String string;
-
     }
 }
