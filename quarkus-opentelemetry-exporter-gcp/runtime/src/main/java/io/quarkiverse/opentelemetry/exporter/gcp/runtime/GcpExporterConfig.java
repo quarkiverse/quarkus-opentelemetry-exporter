35013e7de6d2487bd1fd155f396cb64e0e180efc
brunobat <brunobat@gmail.com>
2024-11-05 16:55:46 +0000

    Make the gcp exporter use ConfigMapping

--- quarkus-opentelemetry-exporter-gcp/runtime/src/main/java/io/quarkiverse/opentelemetry/exporter/gcp/runtime/GcpExporterConfig.java
+++ quarkus-opentelemetry-exporter-gcp/runtime/src/main/java/io/quarkiverse/opentelemetry/exporter/gcp/runtime/GcpExporterConfig.java
@@ -2,37 +2,38 @@ package io.quarkiverse.opentelemetry.exporter.gcp.runtime;
 
 import java.util.Optional;
 
-import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
-import io.quarkus.runtime.annotations.ConvertWith;
 import io.quarkus.runtime.configuration.TrimmedStringConverter;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithConverter;
+import io.smallrye.config.WithDefault;
 
 public class GcpExporterConfig {
-    @ConfigRoot(name = "opentelemetry.tracer.exporter.gcp", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
-    public static class GcpExporterBuildConfig {
+    @ConfigMapping(prefix = "quarkus.opentelemetry.tracer.exporter.gcp")
+    @ConfigRoot(phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
+    public interface GcpExporterBuildConfig {
         /**
          * GCP's Tracing exporter support. Enabled by default.
          */
-        @ConfigItem(defaultValue = "true")
-        public Boolean enabled;
+        @WithDefault(value = "true")
+        public Boolean enabled();
     }
 
-    @ConfigRoot(name = "opentelemetry.tracer.exporter.gcp", phase = ConfigPhase.RUN_TIME)
-    public static class GcpExporterRuntimeConfig {
+    @ConfigMapping(prefix = "quarkus.opentelemetry.tracer.exporter.gcp")
+    @ConfigRoot(phase = ConfigPhase.RUN_TIME)
+    public interface GcpExporterRuntimeConfig {
         /**
          * Override to set GCP's projectid.
          */
-        @ConfigItem
-        @ConvertWith(TrimmedStringConverter.class)
-        public Optional<String> projectid;
+        @WithConverter(TrimmedStringConverter.class)
+        public Optional<String> projectid();
 
         /**
          * Override for GCP TraceEndpoint setting.
          */
-        @ConfigItem
-        @ConvertWith(TrimmedStringConverter.class)
-        public Optional<String> endpoint;
+        @WithConverter(TrimmedStringConverter.class)
+        public Optional<String> endpoint();
 
         /**
          * Support for Cloud Run environments. Set to `true` for Cloud Run deployments.
@@ -40,7 +41,7 @@ public class GcpExporterConfig {
          * Cloud Run <a href="https://cloud.google.com/trace/docs/setup/java-ot#export">doesn't support background processes</a>
          * and `SimpleSpanProcessor` must be used.
          */
-        @ConfigItem(defaultValue = "false")
-        public Boolean cloudrun;
+        @WithDefault(value = "false")
+        public Boolean cloudrun();
     }
 }
