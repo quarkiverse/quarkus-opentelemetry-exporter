0ad5862b2ac81cd7b8e4c1480e1c79ad941b0212
Jean Bisutti <jean.bisutti@gmail.com>
2024-11-06 11:17:36 +0100

    Bump Azure Monitor OpenTelemetry Exporter

--- quarkus-opentelemetry-exporter-azure/runtime/src/main/java/io/quarkiverse/opentelemetry/exporter/azure/runtime/AzureMonitorCustomizer.java
+++ quarkus-opentelemetry-exporter-azure/runtime/src/main/java/io/quarkiverse/opentelemetry/exporter/azure/runtime/AzureMonitorCustomizer.java
@@ -1,8 +1,7 @@
 package io.quarkiverse.opentelemetry.exporter.azure.runtime;
 
+import com.azure.monitor.opentelemetry.exporter.AzureMonitorExporter;
 import jakarta.inject.Singleton;
-
-import com.azure.monitor.opentelemetry.exporter.AzureMonitorExporterBuilder;
 
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdkBuilder;
 import io.quarkus.opentelemetry.runtime.AutoConfiguredOpenTelemetrySdkBuilderCustomizer;
@@ -17,8 +16,7 @@ public class AzureMonitorCustomizer implements AutoConfiguredOpenTelemetrySdkBui
     }
 
     @Override
-    public void customize(AutoConfiguredOpenTelemetrySdkBuilder builder) {
-        new AzureMonitorExporterBuilder().connectionString(azureConnectionString)
-                .install(builder);
+    public void customize(AutoConfiguredOpenTelemetrySdkBuilder sdkBuilder) {
+        AzureMonitorExporter.customize(sdkBuilder, azureConnectionString);
     }
 }
